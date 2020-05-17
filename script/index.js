735835b7ae31e00e93a5f45d4bcd448fac5d47d2
michaelleriche <mikeleriche@gmail.com>
2020-05-17 16:54:49 -0400

    pseudo code

---
+++ script/index.js
@@ -0,0 +1,13 @@
+//load all sounds from webaudio api into array
+//pair array of notes with cells on grid
+//write onclick function to allow note selection / change color of cell 
+//write onclick function to delete note selection / remove color of cell
+//write onclick function to start / stop sequencer
+//write a function to allow tempo change 
+// write function to clear whole sequence
+
+
+///stretch goals
+//save sequences locally
+//change key of song
+//add multiple sequencers, all synced, to have drum sounds, bass sounds, and piano sounds
